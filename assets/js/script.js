caf53547573ad1cee42eb7d10b34ec07593feeda
PCantenys <pacantenys@gmail.com>
2018-11-07 15:17:20 -0300

    Minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
   var owl = $(".owl-carousel");
 
   owl.owlCarousel({
-    items: 1,
-    autoplay: true,
-    nav: true,
-    smartSpeed: 900,
-    navigationText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]
-  });
+     items : 1,
+     loop  : true,
+     margin : 30,
+     nav    : true,
+     smartSpeed :900,
+     navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+   });
 
 });
