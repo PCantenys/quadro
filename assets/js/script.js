728e63a43cb1c5c5bbb2afc00d8a38823180293d
falconSP <pacantenys@gmail.com>
2018-11-06 22:37:57 -0300

    refactoring de codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,42 @@
-//
-// var owl = $('.owl-carousel');
-// owl.owlCarousel({
-//   items: 1,
-//   loop: true,
-//   margin: 30,
-//   nav: true,
-//   smartSpeed :900,
-//   navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
-// });
+$(document).ready(function() {
 
+    // var owl = $(".owl-carousel");
 
-$(".owl-carousel").owlCarousel({
+    // owl.owlCarousel({
+    //   items: 1,
+    //   autoplay: true,
+    //   nav: true,
+    //   smartSpeed: 900,
+    //   // navigationText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+    // });
+
+    $('.owl-carousel').owlCarousel({
+    stagePadding: 0,
     items: 1,
-    autoplay: true,
-    navigation: true,
-    smartSpeed: 900,
-    navigationText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+    loop:true,
+    margin:0,
+    singleItem:true,
+    nav:true,
+    navText: [
+        "<i class='fa fa-caret-left'></i>",
+        "<i class='fa fa-caret-right'></i>"
+    ],
+    dots:true
 });
 
-// $('.play').on('click',function(){
-//     owl.trigger('autoplay.play.owl',[1000]);
-// });
-// $('.stop').on('click',function(){
-// owl.trigger('autoplay.stop.owl');
-// });
 
 
+});
+
+
+// <img class="img-responsive center-block" src="assets/img/003-ecological-energy.png" alt="img-responsive">
+// <h2>Eficiencia Energetica</h2>
+// <p>Iluminacion LED</p>
+// <p>Sist. de refrigeracion de alta eficiencia Termica</p>
 //
-// $(".category-wrapper").owlCarousel({
-//      items : 4,
-//      loop  : true,
-//      margin : 30,
-//      nav    : true,
-//      smartSpeed :900,
-//      navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
-//    });
+//
+// <img class="img-responsive center-block" src="assets/img/001-safe.png" alt="img-responsive">
+// <h2>Seguridad</h2>
+// <p>Todos sus vidrios son templados</p>
+// <p>Cableados sin empalmes</p>
+// <p>Sello de Seguridad Electrica IRAM</p>
