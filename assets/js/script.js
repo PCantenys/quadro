abc0bb422829c91e7e2ba64e69e03a67f1d996a4
PCantenys <pacantenys@gmail.com>
2018-11-07 12:19:14 -0300

    added anchor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,13 @@
 $(document).ready(function() {
 
-    var owl = $(".owl-carousel");
+  var owl = $(".owl-carousel");
 
-    owl.owlCarousel({
-      items: 1,
-      autoplay: true,
-      nav: true,
-      smartSpeed: 900,
-      navigationText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
-    });
-
-
-
-
+  owl.owlCarousel({
+    items: 1,
+    autoplay: true,
+    nav: true,
+    smartSpeed: 900,
+    navigationText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]
+  });
 
 });
-
-
-// <img class="img-responsive center-block" src="assets/img/003-ecological-energy.png" alt="img-responsive">
-// <h2>Eficiencia Energetica</h2>
-// <p>Iluminacion LED</p>
-// <p>Sist. de refrigeracion de alta eficiencia Termica</p>
-//
-//
-// <img class="img-responsive center-block" src="assets/img/001-safe.png" alt="img-responsive">
-// <h2>Seguridad</h2>
-// <p>Todos sus vidrios son templados</p>
-// <p>Cableados sin empalmes</p>
-// <p>Sello de Seguridad Electrica IRAM</p>
